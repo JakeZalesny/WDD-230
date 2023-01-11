ad82df05a59b870e45d91486821df41aa7f279e1
Jake Zalesny <jrzalesny@gmail.com>
2023-01-10 19:49:04 -0800

    added some fun stuff ;)

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,20 @@ document.querySelector('#currentyear').textContent = today.getFullYear();
 //sets last modified date.
 //last modified is a property of the document, its associated with the document 
 document.querySelector('#lastmodified').textContent = document.lastModified; 
+
+function playRighteous() {
+    var audio = document.querySelector('#righteous');
+    audio.play(); 
+}
+
+function stopRighteous() {
+    var audio = document.querySelector('#righteous');
+    audio.pause(); 
+}
+function replaceImageWithGif() {
+    document.getElementById('image').src = './imgs/pepe_lore.gif';
+}
+
+function undoReplacement() {
+    document.getElementById('image').src = './imgs/photo_for_class_resized.jpg';
+}
