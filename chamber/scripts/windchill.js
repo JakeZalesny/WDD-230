78424a78a2b0544e219482c9323b4eab2072e34f
Jake Zalesny <jrzalesny@gmail.com>
2023-02-11 19:44:32 -0800

    changed assignment a bunch

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,19 +1,19 @@
 function showWeather(temp, windspeed){
     //get references to placeholders in the page. 
-    let tempuratureobj = document.querySelector("#temperature");
-    let windspeedobj = document.querySelector("#windspeed"); 
-    let windchillobj = document.querySelector("#windchill");
+    tempuraturespan = document.querySelector("#temperature");
+    windspeedspan = document.querySelector("#windspeed"); 
+    windchillspan = document.querySelector("#windchill");
     //calculate the windchill or N/A
-    let chillmsg = "N/A"
+    let windchill = "N/A"
     if(temp <= 50 && windspeed > 3) {
         let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temp * Math.pow(windspeed, 0.16)));
-        chillmsg = `${chill}`;
+        windchill = `${chill}`;
         // if `${chill}deg&;` use windchillobj.innerHTML; 
     }
     //Update the placeholder values. 
-    tempuratureobj.textContent = temp; 
-    windspeedobj.textContent = windspeed; 
-    windchillobj.textContent = chillmsg; 
+    tempuraturespan.textContent = temp; 
+    windspeedspan.textContent = windspeed; 
+    windchillspan.textContent = windchill; 
 }
 
 showWeather(5, 5);
