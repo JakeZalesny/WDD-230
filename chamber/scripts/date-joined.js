dac58a7c60e97ab46f20857d5f9ec2dc0e6a0667
Jake Zalesny <jrzalesny@gmail.com>
2023-02-27 17:25:31 -0800

    added date-joined and hidden element.

---
+++ chamber/scripts/date-joined.js
@@ -0,0 +1,9 @@
+const todaysDate = new Date(); 
+
+let day = todaysDate.getDay(); 
+let month = todaysDate.getMonth() + 1; 
+let year = todaysDate.getFullYear(); 
+
+let currDate = `${day}-${month}-${year}`;
+
+document.querySelector("#curr_date").value = currDate; 
