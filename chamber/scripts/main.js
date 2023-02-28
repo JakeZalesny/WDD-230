5f91f1639574ebe8b7ffad05b851298b1e0bee9e
Jake Zalesny <jrzalesny@gmail.com>
2023-02-28 08:50:30 -0800

    join page link added

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,4 +1,4 @@
 var messageDate = new Date(); 
-if(messageDate.getDay()==6 || messageDate.getDay()==7){
-    document.querySelector("#meet-greet").classList.add('active');
+if(messageDate.getDay()==1 || messageDate.getDay()==2){
+    document.querySelector("#meet-greet").classList.toggle("active");
 }
