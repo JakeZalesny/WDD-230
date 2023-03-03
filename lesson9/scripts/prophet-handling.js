1a30ab580e007a7bf7fe7cefa811f5016b5a0614
Jake Zalesny <jrzalesny@gmail.com>
2023-03-02 17:47:36 -0800

    finished prophets

--- lesson9/scripts/prophet-handling.js
+++ lesson9/scripts/prophet-handling.js
@@ -1,10 +1,15 @@
 const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json"
 
 async function getProphetData() {
-    const response = fetch(url);
+    const response = await fetch(url);
     if(response.ok) {
         const data = await response.json();
         displayProphets(data.prophets);
+    }
+    else {
+        console.error("There was a problem fetching the data");
+        const cards = document.querySelector("div.cards");
+        cards.innerHTML = `<section><h1>There was an error loading the data</h1></section>`;
     }
 }
 
@@ -12,7 +17,7 @@ getProphetData();
 
 const displayProphets = (prophets) => {
     const cards = document.querySelector("div.cards");
-    prophets.forEach(prophet => {
+    prophets.forEach((prophet) => {
         let card = document.createElement("section");
         card.innerHTML = `<h2> ${prophet.name} ${prophet.lastname}</h2>
         <h3>Date of Birth: ${prophet.birthdate}</h3>
