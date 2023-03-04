f01c634f306414635505632705e7fa2db420bb0c
Jake Zalesny <jrzalesny@gmail.com>
2023-03-03 17:50:56 -0800

    fixed like 90% of the problems I was having

--- chamber/scripts/last-visit.js
+++ chamber/scripts/last-visit.js
@@ -2,14 +2,16 @@ const MILLIS_PER_DAY = 24 * 60 * 60 * 1000;
 //represents # of miliseconds from Jan 1, 1970
 let today = new Date(); 
 let lastVisitString = localStorage.getItem("lastVisit");
-let visitspan = document.querySelector('#days-since-visit');
+let visitspan = document.querySelector('#days-since-last-visit');
 if(lastVisitString == null) {
     visitspan.textContext = "0";
 }
 
 else{
     lastVisitDate = new Date(lastVisitString);
-    daysSinceLastVisit = Math.floor((today.getTime() - lastVisitDate.getTime()) / MILLIS_PER_DAY);
-    visitspan.textContext = daysSinceLastVisit; 
+    var daysSinceLastVisit = Math.floor((today.getTime() - lastVisitDate.getTime()) / MILLIS_PER_DAY);
+    visitspan.textContent = daysSinceLastVisit;
+    console.log(daysSinceLastVisit); 
 }
-localStorage.setItem(daysSinceLastVisit);
+localStorage.setItem("lastVisit", lastVisitString);
+// visitspan.textContent = "1";
--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,4 +1,4 @@
 var messageDate = new Date(); 
-if(messageDate.getDay()==1 || messageDate.getDay()==2){
+if(messageDate.getDay()==5 || messageDate.getDay()==2){
     document.querySelector("#meet-greet").classList.toggle("active");
 }
