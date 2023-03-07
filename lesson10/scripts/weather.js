ad82c546022cf73adcd2ad5e8a984ab7610a6b75
Jake Zalesny <jrzalesny@gmail.com>
2023-03-07 09:57:09 -0800

    made weather API

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,40 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+// South and West are negative. 
+const lat = '64.8401';
+const lon = '-147.7200';
+const APIkey = 'e4c19171f0361e7e5340d9c9ef1ccd92';
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=imperial`;
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        //console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML= `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+
+    const desc = weatherData.weather[0].description; 
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc; 
+    //wind speed = data.wind.speed
+}
+  
+  apiFetch();
---
+++ notes/Weather-API.md
@@ -0,0 +1,9 @@
+# Weather API & Web Usability
+
+1. Availability: Always available to the User. 
+2. Clarity: User can find what they need. 
+3. Learnability: Keeping the same layout to the website. 
+4. Credibility: Gotta trust the website. 
+5. Relevance: Knowing customer's needs and what they're looking for. 
+6. Reliability: 5 9's ability, 99.999% up time, 1 sec of down time per day. 99% is 14m 24s of down time. 
+
