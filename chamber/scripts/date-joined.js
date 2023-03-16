ea7c6fc96eb5346284b60dc4a17060c3d638c8ec
Jake Zalesny <jrzalesny@gmail.com>
2023-03-16 09:18:13 -0700

    added hours and minutes to the site.

--- chamber/scripts/date-joined.js
+++ chamber/scripts/date-joined.js
@@ -2,9 +2,11 @@ const todaysDate = new Date();
 
 let day = todaysDate.getDay(); 
 let month = todaysDate.getMonth() + 1; 
-let year = todaysDate.getFullYear(); 
+let year = todaysDate.getFullYear();
+let hours = todaysDate.getHours(); 
+let minutes = todaysDate.getMinutes(); 
 
-let currDate = `${day}-${month}-${year}`;
+let currDate = `${day}-${month}-${year} ${hours}:${minutes}`;
 
 document.querySelector("#curr_date").value = currDate; 
 
