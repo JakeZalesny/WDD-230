01bcb4e0a29e4eeb1e2dd10580436f674d07fe0d
Jake Zalesny <jrzalesny@gmail.com>
2023-03-30 10:15:02 -0700

    Added a bunch of changes

---
+++ bountiful-foods/scripts/hamburger-menu.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    document.querySelector("#navbar").classList.toggle('menu-active')
+    document.querySelector("#menu-close").classList.toggle('menu-active')
+    document.querySelector("#menu-open").classList.toggle('menu-active')
+}
+
+document.querySelector("#hamburger-menu").onclick = toggleMenu; 
+
+const fullDate = new Intl.DateTimeFormat("en-US", {dateStyle: "full"}).format(new Date(document.lastModified));
+
+document.querySelector("#last-modified").textContent = "Last Modified: " + fullDate;
